9c8197b16ccccd1ddf9e4c7ee6fcca98326856cc
Cyro <cyro.thadeu@gmail.com>
2016-07-31 11:52:42 -0300

    areas

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -8,10 +8,11 @@ $(window).scroll(function () {
 $(function () {
     $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 1000, 'easeInOutExpo');
+        $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 700);
         event.preventDefault();
     });
 });
 $('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggle:visible').click();
 });
+
