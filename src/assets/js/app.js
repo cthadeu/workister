610c9b86da77d42f0311bfed0625e41c05784332
Cyro <cyro.thadeu@gmail.com>
2016-07-31 22:50:15 -0300

    area conciliador

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,12 +7,68 @@
 
         $scope.form = "";
         $scope.data = [];
+        $scope.email = "";
+        $scope.senha = "";
+
+        $scope.login = function(){
+            if ($scope.email == "cliente@email.com")
+                window.location = "admin.html";
+
+            if ($scope.email == "conciliador@email.com")
+                window.location = 'admin_conciliador.html'
+        }
 
   }]);
 
     deeal.controller('AdminController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
 
         $scope.form = "demandas.html";
+        $scope.deals = [];
+        $scope.empresas = [];
+
+        $http.get('./assets/data/empresas.json').then(function(response) {
+            $scope.empresas = response.data;
+        });
+
+        $http.get('./assets/data/deeal.json').then(function(response) {
+            $scope.deals = response.data;
+        });
+
+        $http.get('./assets/data/conciliador.json').then(function(response) {
+            $scope.conciliadores = response.data;
+        });
+
+        $scope.selecionarConciliador = function(name){
+            $scope.conciliadorSelecionado = name;
+        }
+
+        $scope.goTo = function(item) {
+            $scope.form = item;
+        }
+        $scope.cssStatusDemanda = function(status) {
+            if (status == 'em_andamento')
+                return 'label-info';
+            if (status == 'em_negociacao')
+                return 'label-warning'
+            if (status == 'finalizada')
+                return 'label-success'
+        }
+
+        $scope.descreveStatus = function(status) {
+            if (status == 'em_andamento')
+                return 'Em Andamento';
+            if (status == 'em_negociacao')
+                return 'Em Negociação'
+            if (status == 'finalizada')
+                return 'Finalizada'
+        }
+
+    }]);
+
+
+    deeal.controller('AdminConciliadorController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+
+        $scope.form = "demandas_conciliador.html";
         $scope.deals = [];
         $scope.conciliadores = [];
         $scope.conciliadorSelecionado = "";
@@ -56,4 +112,5 @@
         }
 
     }]);
+
 })(window.angular);
